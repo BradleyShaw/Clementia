08c58319abb02288c6490da10189d53609589044
Bradley Shaw <bs@sigint.pw>
2017-10-03 21:01:30 +0100

    Add playurl

---
+++ commands/playurl.js
@@ -0,0 +1,26 @@
+module.exports = async function(bot, message, args) {
+  if (message.author.id !== bot.config.owner) {
+    message.reply('You are not authorized to use this command.');
+    return;
+  }
+
+  if (!args[0]) {
+    message.reply('Usage: playurl <url>');
+    return;
+  }
+
+  const voiceChannel = message.guild.channels.find(val => val.name === 'Music');
+  
+  if (!voiceChannel) {
+    message.reply('This Guild has no Music channel');
+    return;
+  }
+
+  const connection = (voiceChannel.members.has(bot.client.user.id)
+                      ? voiceChannel.connection
+                      : await voiceChannel.join());
+  
+  const dispatcher = await connection.playArbitraryInput(args[0]);
+  dispatcher.on('error', e => bot.log.error(e));
+  dispatcher.on('end', () => voiceChannel.leave());
+}
